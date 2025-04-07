ecfaf0c6c61bc2ab387b5eef242012a084b2b99b
jui-pixel <absta.ee13@nycu.edu.tw>
2025-04-07 19:35:04 +0800

    refactor Lab08.py to improve variable naming and enhance readability

--- else_folder/Lab08.py
+++ else_folder/Lab08.py
@@ -1,11 +1,12 @@
 import time
 
-names = ["john doe", "jane smith", "bob johnson"]   # List of names
-totalppl = 0                                        # Initialize total people count
-for name in names:                                  # Loop through each name
-    split_name = name.split()                       # Split the name into first and last
-    if len(split_name) == 2:                        # Check if there are two parts
-        print(split_name[0] + " " + split_name[1])  # Print the name
-        totalppl += 1                               # Increment the people count
-    time.sleep(1)                                   # Sleep for 1 second
-print("total: " + str(totalppl))                    # Print total count
+names = ["john doe", "jane smith", "bob johnson"]
+valid_name_count = 0
+for name in names:
+    split_name = name.split()
+    if len(split_name) == 2:
+        forward_name, last_name = split_name[0], split_name[1]
+        print(f"{forward_name} {last_name}")
+        valid_name_count += 1
+    time.sleep(1)
+print("total: " + str(valid_name_count))
